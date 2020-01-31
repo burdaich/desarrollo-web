c17ef8db6d00999006fa706ea27b79b2f0b5b6e5
rodriguez <e.rodriguez@ariadnagrid.com>
2020-01-31 12:36:51 +0100

    Responsive Design video 22 --> EventListener on menu.

---
+++ index.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('.menu');
+const burgerButton = document.querySelector('#burger-menu');
+
+burgerButton.addEventListener('click', hideShow);
+
+function hideShow() {
+    if (menu.classList.contains('is-active')) {
+        menu.classList.remove('is-active')
+    } else {
+        menu.classList.add('is-active');
+    }
+}
